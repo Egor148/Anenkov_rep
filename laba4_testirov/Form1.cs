34e648e99ab06c09128f06542cca3047dc65f070
EgorAn <57406043+Egor148@users.noreply.github.com>
2022-09-07 19:10:43 +0700

    функция экстраполяции

--- laba4_testirov/Form1.cs
+++ laba4_testirov/Form1.cs
@@ -175,6 +175,11 @@ namespace laba4_testirov
             }
         }
 
+        public double extrapolate(double[,] d, double x)
+        {
+            double y = d[0, 1] + (x - d[0, 0]) / (d[1, 0] - d[0, 0]) * (d[1, 1] - d[0, 1]);
+            return y;
+        }
 
     }
 }
