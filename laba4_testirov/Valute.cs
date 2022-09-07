fe15eb3c365811f49b5c7e61ebf3fde370441bfe
EgorAn <57406043+Egor148@users.noreply.github.com>
2022-09-07 18:58:50 +0700

    create class Valute

--- laba4_testirov/Valute.cs
+++ laba4_testirov/Valute.cs
@@ -8,5 +8,9 @@ namespace laba4_testirov
 {
     class Valute
     {
+        public string[] name = new[] { "USD", "EUR", "RUB", "BYN", "INR", "KZT", "CAD", "CNY", "UZS" };
+        public string id; //номер валюты
+        public List<string> his = new List<string>();
+        public List<string> date = new List<string>();
     }
 }
