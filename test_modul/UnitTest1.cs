2ebba74a93be9d5a03525c321f31332a78ba4f25
EgorAn <57406043+Egor148@users.noreply.github.com>
2022-09-07 19:01:21 +0700

    test_readKurs

--- test_modul/UnitTest1.cs
+++ test_modul/UnitTest1.cs
@@ -15,5 +15,18 @@ namespace test_modul
             double expected = 77.5;
             Assert.AreEqual(expected, f.extrapolate(d, 4));
         }
+        [TestMethod]
+        public void read_kursTest()
+        {
+            Form1 f = new Form1();
+            double c1, c2, n;
+            c1 = 25;
+            c2 = 5;
+            n = 3;
+            double expected = 15;
+            double res = f.read_kurs(c1, c2, n);
+            Assert.AreEqual(expected, res);
+
+        }
     }
 }
