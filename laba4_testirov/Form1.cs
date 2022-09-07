4394d20b09a15f08bf5afbec27642a85c8d92801
EgorAn <57406043+Egor148@users.noreply.github.com>
2022-09-07 19:02:43 +0700

    create_obkect_class

--- laba4_testirov/Form1.cs
+++ laba4_testirov/Form1.cs
@@ -12,9 +12,16 @@ namespace laba4_testirov
 {
     public partial class Form1 : Form
     {
+        double c1, c2;
+        Valute val = new Valute();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
 
         private void button1_Click(object sender, EventArgs e)
